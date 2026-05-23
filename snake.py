41589b1d96d9650352984b4a3f3f8ffd006d9c55
Alexandru <lexyus22@gmail.com>
2026-05-23 19:14:13 +0200

    Add snake keyboard controls.

--- snake.py
+++ snake.py
@@ -1,13 +1,21 @@
 import turtle as t
+from xml.dom.xmlbuilder import DOMEntityResolver
 
 POSITION = [(0,0), (-20,0), (-40,0)]
+
 MOVE = 20
+
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
 
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for each_position in POSITION:
@@ -23,4 +31,21 @@ class Snake:
             new_y = self.segments[seg_n - 1].ycor()
             self.segments[seg_n].goto(new_x, new_y)
         self.segments[0].forward(MOVE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
 
