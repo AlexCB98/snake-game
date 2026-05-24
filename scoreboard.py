39d3deda3cf012f0f0e156227f9939be7469d7d4
Alexandru <lexyus22@gmail.com>
2026-05-24 11:28:42 +0200

    Implement wall collision and game over.

--- scoreboard.py
+++ scoreboard.py
@@ -10,12 +10,17 @@ class Scoreboard(Turtle):
         self.score = 0
         self.color('gold')
         self.penup()
-        self.goto(-240, 255)
+        self.goto(-220, 255)
         self.hideturtle()
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.write(f'Score: {self.score}', align=ALIGN, font=FONT)
+
+    def game_over(self):
+        self.goto(0,0)
+        self.color('red')
+        self.write('GAME OVER', align=ALIGN, font=FONT)
 
     def increase_score(self):
         self.score += 1
